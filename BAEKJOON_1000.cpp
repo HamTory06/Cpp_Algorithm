2414b66ca9c0cb6d9c479ba7c5a6def7acb1682f
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-03-09 21:47:52 +0900

    3/9 - 1000

---
+++ BAEKJOON_1000.cpp
@@ -0,0 +1,13 @@
+//
+// Created by HamTory on 2023/03/09.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 0;
+    int b = 0;
+    cin>>a;
+    cin>>b;
+    cout << a+b << endl;
+}
