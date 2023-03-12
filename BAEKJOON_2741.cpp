b4126d198d8cc09d427502c2c3d2fe8daa53771f
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-03-12 18:35:14 +0900

    3/12 - BAEKJOON_2741.cpp

---
+++ BAEKJOON_2741.cpp
@@ -0,0 +1,16 @@
+//
+// Created by HamTory on 2023/03/12.
+//
+#include "iostream"
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cout << i << '\n';
+        //endl 말고 \n 사용하자
+    }
+    return 0;
+}
