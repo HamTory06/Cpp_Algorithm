f582298423a84322424c8015b9919f9471449d4f
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-03-18 17:44:17 +0900

    3/18 - BAEKJOON_2445.cpp - BAEKJOON_1009.cpp

--- BAEKJOON_2445.cpp
+++ BAEKJOON_2445.cpp
@@ -8,8 +8,30 @@ using namespace std;
 
 int main(){
     int n;
-    for(int i=0; i<n*2-1; i++){
-
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        for(int j=0; j<n*2-i*2-2; j++){
+            cout << ' ';
+        }
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    for(int i=1; i<=n-1; i++){
+        for(int j=0; j<n-i; j++){
+            cout << "*";
+        }
+        for(int j=0; j<i*2; j++){
+            cout << ' ';
+        }
+        for(int j=0; j<n-i; j++){
+            cout << "*";
+        }
+        cout << '\n';
     }
     return 0;
 }
